02638d8ab7f7b649f6b1744c04edb0f0c330d42d
CHAE <cowhdals1111@gmail.com>
2024-07-23 19:59:57 +0900

    🎉 Init: 초기세팅

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,13 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    screens: {
+      sm: { max: "480px" },
+      md: "481px",
+      lg: "769px",
+      xl: "1280px",
     },
+    extend: {},
   },
   plugins: [],
 };
