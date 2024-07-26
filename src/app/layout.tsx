a18f7803530e687c2a0009263875a3d121557688
JHmeatschool <ceojr1234@gmail.com>
2024-07-26 16:01:10 +0900

    ✨ Feat: navbar 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import Navbar from "@/components/common/NavBar/navbar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/common/NavBar/navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <header className="sticky top-0 z-10 h-60 border-b border-border-primary bg-background-secondary">
+      네비게이션바
+    </header>
+  );
+}
