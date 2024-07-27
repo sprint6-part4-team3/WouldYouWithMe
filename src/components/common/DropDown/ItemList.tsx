4cff1ada6434d90574798cbc640171404deb855e
easyhyun00 <leejihyun0324@gmail.com>
2024-07-27 21:42:17 +0900

    🔨 Refactor: 드롭다운 아이템리스트 위치 props 추가

--- src/components/common/DropDown/ItemList.tsx
+++ src/components/common/DropDown/ItemList.tsx
@@ -6,13 +6,22 @@ interface DropDownItemListProps {
   children: ReactNode;
   /** 드롭다운 메뉴 open 여부입니다.  */
   isOpen: boolean;
+  /** 드롭다운 메뉴 top 위치입니다. 기본값 top-30 */
+  topPosition?: string;
+  /** 드롭다운 메뉴 right 위치입니다. 기본값 right-0 */
+  rightPosition?: string;
 }
 
-const DropDownItemList = ({ children, isOpen }: DropDownItemListProps) => (
+const DropDownItemList = ({
+  children,
+  isOpen,
+  topPosition = "top-30",
+  rightPosition = "right-0",
+}: DropDownItemListProps) => (
   <AnimatePresence>
     {isOpen && (
       <motion.div
-        className="absolute right-0 top-30 z-10 w-120 flex-col rounded-12 border border-border-primary bg-background-secondary text-text-primary shadow-md "
+        className={`${topPosition} ${rightPosition} absolute right-0 top-30 z-10 w-120 flex-col rounded-12 border border-border-primary bg-background-secondary text-text-primary shadow-md`}
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }}
         exit={{ opacity: 0, scale: 0 }}
