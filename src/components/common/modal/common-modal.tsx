230f044f26697213a9dcdcb8533ee8bcefde824d
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2024-07-28 16:07:11 +0900

    🔨 Refactor: 코드리뷰를 반영하여 CreatePortal 사용

--- src/components/common/modal/common-modal.tsx
+++ src/components/common/modal/common-modal.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import React, { ReactNode } from "react";
+import { createPortal } from "react-dom";
 
 import { IconAlert, IconX } from "@/public/assets/icons";
 
@@ -24,55 +25,56 @@ const Modal: React.FC<ModalProps> = ({
   className,
   children,
 }) => {
-  if (!isOpen) return null;
-
   const titleMarginClass = description ? "mb-8" : "mb-24";
 
-  return (
-    <div className="fixed inset-0 flex size-full items-center justify-center overflow-y-auto bg-background-primary/50">
-      <div
-        className={clsx(
-          "relative w-384 rounded-8 bg-background-secondary px-48 pb-32 pt-48 shadow-lg",
-          className,
-        )}
-      >
-        {showCloseButton && (
-          <button
-            type="button"
-            onClick={onClose}
-            className="absolute right-12 top-12 text-text-secondary hover:text-text-primary"
+  return isOpen
+    ? createPortal(
+        <div className="fixed inset-0 flex size-full items-center justify-center overflow-y-auto bg-background-primary/50">
+          <div
+            className={clsx(
+              "relative w-384 rounded-8 bg-background-secondary px-48 pb-32 pt-48 shadow-lg",
+              className,
+            )}
           >
-            <IconX width={20} height={20} />
-          </button>
-        )}
+            {showCloseButton && (
+              <button
+                type="button"
+                onClick={onClose}
+                className="absolute right-12 top-12 text-text-secondary hover:text-text-primary"
+              >
+                <IconX width={20} height={20} />
+              </button>
+            )}
 
-        <div className="flex flex-col items-center">
-          {showWarningIcon && (
-            <div className="mb-16">
-              <IconAlert width={24} height={24} />
-            </div>
-          )}
-
-          {title && (
-            <h2
-              className={clsx(
-                "font-pretendard leading-19 text-center text-16 font-medium text-text-primary",
-                titleMarginClass,
+            <div className="flex flex-col items-center">
+              {showWarningIcon && (
+                <div className="mb-16">
+                  <IconAlert width={24} height={24} />
+                </div>
               )}
-            >
-              {title}
-            </h2>
-          )}
-          {description && (
-            <p className="font-pretendard leading-17 mb-24 text-center text-14 font-medium text-text-secondary">
-              {description}
-            </p>
-          )}
-          {children}
-        </div>
-      </div>
-    </div>
-  );
+
+              {title && (
+                <h2
+                  className={clsx(
+                    "font-pretendard leading-19 text-center text-16 font-medium text-text-primary",
+                    titleMarginClass,
+                  )}
+                >
+                  {title}
+                </h2>
+              )}
+              {description && (
+                <p className="font-pretendard leading-17 mb-24 text-center text-14 font-medium text-text-secondary">
+                  {description}
+                </p>
+              )}
+              {children}
+            </div>
+          </div>
+        </div>,
+        document.body,
+      )
+    : null;
 };
 
 export default Modal;
