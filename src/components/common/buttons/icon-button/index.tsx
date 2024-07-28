829a74850220f53852033b71fd210dae62ed2404
CHAE <cowhdals1111@gmail.com>
2024-07-28 14:48:32 +0900

    ✨ Feat: 범용 아이콘 버튼

--- src/components/common/buttons/icon-button/index.tsx
+++ src/components/common/buttons/icon-button/index.tsx
@@ -1,79 +1,56 @@
 "use client";
 
 import { clsx } from "clsx";
-import { ComponentProps } from "react";
+import { ComponentProps, ReactNode } from "react";
 
-interface ButtonProps extends ComponentProps<"button"> {
+interface IconButtonProps extends ComponentProps<"button"> {
   children: string;
   colorType: "primary" | "white" | "danger" | "secondary" | "noFill";
+  Icon: ReactNode;
 }
 
 /**
  * 
- * 커스텀 버튼 컴포넌트 radius(12px)
+ * 커스텀 범용 아이콘 버튼 컴포넌트(radius:40px)
  * colorType에 따라 디자인이 적용됩니다.
- * 글자 크기 16px 굵기 600 중앙정렬입니다.
- * 버튼 가로 세로 크기를 className으로 주면 됩니다.
+ * svg를 리액트 컴포넌트로 바꿔서 넣어줍니다.("@svgr/webpack")
+ * 글자는 중앙정렬입니다.
+ * 글자와 아이콘 사이 거리는 4px입니다.
+ * 버튼 가로,세로 + 글씨 크기,굵기는 className으로 주면 됩니다.
  * 커스텀 디자인 적용을 원하는 경우에도 className으로 주면 덮어 쓸 수 있습니다.
  * button attributes 모두 사용 가능합니다.
  * @example
- *  <Button
+ * import IconPlus from "@/public/assets/icons/icon-plus.svg";
+ *  <IconButton
+      Icon={<IconPlus />}
       colorType="primary"
-      className="h-48 w-280"
-      onClick={() => {}}
-      type="submit"
+      className="h-83 w-280 text-15"
     >
-      로그인
-    </Button>
-
- *  <Button
-      colorType="white"
-      className="h-83 w-280"
-      onClick={() => {}}
-    >
-      닫기
-    </Button>
-
-    <Button
-      colorType="danger"
-      className="h-83 w-280"
-      onClick={() => {}}
-    >
-      삭제하기
-    </Button>
-
-    <Button
-      colorType="secondary"
-      className="h-83 w-280"
-      onClick={() => {}}
-    >
-      닫기
-    </Button>
-    
-    <Button
-      colorType="noFill"
-      className="h-83 w-280"
-      onClick={() => {}}
-    >
-      수정하기
-    </Button>
+      추가하기
+    </IconButton>
   * @param children 버튼 안에 들어갈 글자
   * @param colorType 
   *   "primary"(primary 배경색+하얀 글자) | 
   *   "white"(하얀 배경색+primary 글자 ) | 
   *   "danger"(빨간 배경색+ 하얀 글자) | 
-  *   "secondary"(하얀 배경색+ 회색 글자) |
+  *   "secondary"(하얀 배경색+ 회색 글자)
   *   "noFill"(투명배경+primary 글자)
   * @param className 기본적으로 넓이, 높이, 반응형 + 커스텀 
   * @param rest onClick,type 등 버튼 속성 사용 가능
   * @author ☯️채종민
   */
 
-const Button = ({ children, colorType, className, ...rest }: ButtonProps) => (
+const IconButton = ({
+  children,
+  colorType,
+  className,
+  Icon,
+  ...rest
+}: IconButtonProps) => (
   <button
     type="button"
     className={clsx(
-      "flex cursor-pointer items-center justify-center rounded-12 text-lg-semibold disabled:cursor-not-allowed",
+      "flex cursor-pointer items-center justify-center gap-4 rounded-40 disabled:cursor-not-allowed",
       {
         "bg-brand-primary text-text-inverse hover:bg-interaction-hover disabled:bg-interaction-inactive":
           colorType === "primary",
@@ -90,8 +67,9 @@ const Button = ({ children, colorType, className, ...rest }: ButtonProps) => (
     )}
     {...rest}
   >
-    {children}
+    {Icon}
+    <span>{children}</span>
   </button>
 );
 
-export default Button;
+export default IconButton;
