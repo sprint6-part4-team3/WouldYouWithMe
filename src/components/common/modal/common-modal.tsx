657b7a0ba984e2fde1f625d50fcb9581639a1dc2
HANSANGWOO <99471821+Han-wo@users.noreply.github.com>
2024-07-29 12:42:36 +0900

    ✨Feat(#8) 공용모달컴포넌트 (#20)
    * 📦️ Chore: 린트 설정 일부 변경 및 tsconfig 수정
    
    pxToRem 설정을 머지후 설치가 되었음에도 설치가 필요하다는 오류린트 제거
    
    * ✨ Feat: 공용모달 컴포넌트 제작
    
    * 🔨 Refactor: 테일윈드버전에 따라 달라진 배경투명도 방식 수정
    
    * 🔨 Refactor: 모달내에 description 이 있을때와 없을때의 마진 수정
    
    * 🔨 Refactor: 코드리뷰 반영하여 함수를 상수로 변경
    
    * 🔨 Refactor: 코드리뷰를 반영하여 CreatePortal 사용
    
    * 📦️ Chore: 일부 린트설정 수정
    
    * ✨ Feat: 외부클릭 감지 추가
    
    추가로 z-index, useClient 사용으로 클라이언트 컴포넌트로 지정
    
    * 📦️ Chore: 스토리북 props 주석 설정
    
    * 📦️ Chore: 스토리북 주석 삭제 및 jsDoc추가
    
    * ✨ Feat: Framer Motion 추가
    
    * 🔨 Refactor: 중복되는 스토리 제거
    
    ---------
    
    Co-authored-by: han-wo98 <99472476+han-wo98@users.noreply.github.com>

---
+++ src/components/common/modal/common-modal.tsx
@@ -0,0 +1,184 @@
+"use client";
+
+import clsx from "clsx";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { ReactNode, useRef } from "react";
+import { createPortal } from "react-dom";
+
+import { IconAlert, IconX } from "@/public/assets/icons";
+
+export interface ModalProps {
+  /**
+   * 모달의 열림/닫힘 상태를 제어합니다.
+   */
+  isOpen: boolean;
+
+  /**
+   * 모달이 닫힐 때 호출되는 함수입니다.
+   */
+  onClose: () => void;
+
+  /**
+   * 모달의 제목을 설정합니다.
+   */
+  title: string;
+
+  /**
+   * 모달의 설명 텍스트를 설정합니다.
+   */
+  description?: string;
+
+  /**
+   * 모달 닫기 버튼의 표시 여부를 설정합니다.
+   * 기본설정은 `false`입니다.
+   */
+  showCloseButton?: boolean;
+
+  /**
+   * 경고 아이콘의 표시 여부를 설정합니다.
+   * 기본설정은 `false`입니다.
+   */
+  showWarningIcon?: boolean;
+
+  /**
+   * 모달에 추가적인 CSS 클래스를 적용합니다.
+   */
+  className?: string;
+
+  /**
+   * 모달 내부에 표시될 자식 컴포넌트들입니다.
+   */
+  children?: ReactNode;
+}
+
+const modalVariants = {
+  hidden: {
+    opacity: 0,
+    scale: 0.9,
+    y: -20,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      damping: 20,
+      stiffness: 300,
+      mass: 0.5,
+    },
+  },
+  exit: {
+    opacity: 0,
+    scale: 0.95,
+    transition: {
+      type: "tween",
+      ease: "easeInOut",
+      duration: 0.2,
+    },
+  },
+};
+
+const overlayVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 0.2 } },
+  exit: { opacity: 0, transition: { duration: 0.2 } },
+};
+
+/**
+ * 모달 컴포넌트입니다.
+ * 페이지 위에 오버레이와 함께 내용을 표시합니다.
+ * `isOpen`,
+  `onClose`,
+  `title`,
+  `description`,
+  `showCloseButton` = `false`,
+  `showWarningIcon` = `false`,
+  `className`,
+  `children`,
+  속성이 있습니다.
+ */
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  title,
+  description,
+  showCloseButton = false,
+  showWarningIcon = false,
+  className,
+  children,
+}) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const titleMarginClass = description ? "mb-8" : "mb-24";
+
+  return createPortal(
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          ref={modalRef}
+          onClick={(e) => e.target === modalRef.current && onClose()}
+          className="fixed inset-0 z-50 flex size-full items-center justify-center overflow-y-auto bg-background-primary/50"
+          variants={overlayVariants}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+        >
+          <motion.div
+            className={clsx(
+              "z-51 relative w-384 rounded-8 bg-background-secondary px-48 pb-32 pt-48 shadow-lg",
+              className,
+            )}
+            variants={modalVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          >
+            {showCloseButton && (
+              <button
+                type="button"
+                onClick={onClose}
+                className="absolute right-12 top-12 text-text-secondary hover:text-text-primary"
+              >
+                <IconX
+                  width={20}
+                  height={20}
+                  className="text-gray-500 transition-colors duration-200 hover:text-blue-500"
+                  transition-colors
+                />
+              </button>
+            )}
+
+            <div className="flex flex-col items-center">
+              {showWarningIcon && (
+                <div className="mb-16">
+                  <IconAlert width={24} height={24} />
+                </div>
+              )}
+
+              {title && (
+                <h2
+                  className={clsx(
+                    "font-pretendard leading-19 text-center text-16 font-medium text-text-primary",
+                    titleMarginClass,
+                  )}
+                >
+                  {title}
+                </h2>
+              )}
+              {description && (
+                <p className="font-pretendard leading-17 mb-24 text-center text-14 font-medium text-text-secondary">
+                  {description}
+                </p>
+              )}
+              {children}
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>,
+    document.body,
+  );
+};
+
+export default Modal;
