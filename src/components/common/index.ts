4d9230314c170ab6136553c08efa237aa24bed54
easyhyun00 <leejihyun0324@gmail.com>
2024-07-29 15:35:53 +0900

    🔥 Remove: 공통컴포넌트 index.ts 삭제

--- src/components/common/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as DropDown } from "./drop-down";
-export { default as ErrorMessage } from "./form/error-message";
-export { default as FieldWrapper } from "./form/field-wrapper";
-export { default as Input } from "./form/input";
-export { default as Textarea } from "./form/textarea";
-export { default as Modal } from "./modal";
-export { default as NavBar } from "./nav-bar";
