4183682e690a24efc9aebd9ef6c295bca0f77ba1
JHmeatschool <ceojr1234@gmail.com>
2024-07-29 22:20:54 +0900

    ✨ Feat: 팀 추가하기 버튼 추가

--- src/components/common/nav-bar/nav-component.tsx
+++ src/components/common/nav-bar/nav-component.tsx
@@ -7,6 +7,7 @@ import useToggle from "@/hooks/use-toggle";
 import { IconDropdown, IconUser } from "@/public/assets/icons";
 import LogoImage from "@/public/assets/images/logo-coworkers.png";
 
+import PlusButton from "../buttons/plus-button";
 import DropDown from "../drop-down";
 
 interface TeamDropdownProps {
@@ -49,7 +50,13 @@ const TeamDropdown = ({ teamName }: TeamDropdownProps) => {
               <span className="ml-12">마케팅 팀</span>
             </div>
           </DropDown.Item>
-          <DropDown.Item>팀추가하기버튼</DropDown.Item>
+          <DropDown.Item>
+            <div className="flex justify-center">
+              <PlusButton className="h-40 w-130 rounded-10 border border-border-primary bg-transparent">
+                팀 추가하기
+              </PlusButton>
+            </div>
+          </DropDown.Item>
         </DropDown.Menu>
       </DropDown>
     </div>
