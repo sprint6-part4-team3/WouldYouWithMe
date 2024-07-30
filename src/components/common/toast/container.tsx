f4c2cc60bf7272564b22e892c453efaf0e215dcd
easyhyun00 <leejihyun0324@gmail.com>
2024-07-30 13:33:41 +0900

    🚑️ Fix: document 오류 수정

--- src/components/common/toast/container.tsx
+++ src/components/common/toast/container.tsx
@@ -3,6 +3,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { AnimatePresence } from "framer-motion";
 import { useAtomValue } from "jotai";
+import { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 import { ToastListAtom } from "@/stores/toast";
@@ -10,7 +11,16 @@ import { ToastListAtom } from "@/stores/toast";
 import Toast from ".";
 
 const ToastContainer = () => {
+  const [isClient, setIsClient] = useState(false);
   const toasts = useAtomValue(ToastListAtom);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
 
   return ReactDOM.createPortal(
     <div className="absolute left-1/2 top-30 flex -translate-x-1/2 flex-col items-center gap-12 sm:w-full sm:px-12 md:min-w-400">
