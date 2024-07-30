5ae95d002a8bcfe2ac359a0ba5079ae358560072
CHAE <cowhdals1111@gmail.com>
2024-07-30 16:39:41 +0900

    🔨 Refactor: className prop 추가

--- src/components/common/framer-motion/motion/index.tsx
+++ src/components/common/framer-motion/motion/index.tsx
@@ -8,6 +8,7 @@ type AnimationType = "fade-in";
 interface MotionProps extends FramerMotionProps {
   children: ReactNode;
   animation?: AnimationType;
+  className?: string;
 }
 
 const animations: Record<AnimationType, Partial<FramerMotionProps>> = {
@@ -22,6 +23,8 @@ const animations: Record<AnimationType, Partial<FramerMotionProps>> = {
  * Framer Motion 컴포넌트
  * 서버 컴포넌트에서 framer-motion의 motion을 사용하고 싶을 때 사용하세요.
  * framer-motion을 적용할 곳에 감싸서 사용하면 됩니다.
+ * motion.div로 감싸지며, 크기는 자식 요소에 딱 맞게 늘어납니다.
+ * 추가하고 혹시 className 추가하고 싶으면 넣을 수 있습니다.
  * framer-motion motion 속성 전부 사용 가능하며, 자주 쓰는 애니메이션은 animation prop으로 쉽게 사용 가능합니다.
  * @example
  *  <Motion animation="fade-in" >
@@ -39,14 +42,19 @@ const animations: Record<AnimationType, Partial<FramerMotionProps>> = {
  *
  * @param children ReactNode
  * @param animation "fade-in" | (추가 애니메이션 타입)
+ * @param className 추가로 className 넣고 싶을 때
  * @param rest framer-motion의 motion 속성 사용 가능
  * @author ☯️채종민
  */
-const Motion = ({ children, animation, ...rest }: MotionProps) => {
+const Motion = ({ children, animation, className, ...rest }: MotionProps) => {
   const animationProps = animation ? animations[animation] : {};
 
   return (
-    <motion.div className="size-fit" {...animationProps} {...rest}>
+    <motion.div
+      className={`size-fit ${className || ""}`}
+      {...animationProps}
+      {...rest}
+    >
       {children}
     </motion.div>
   );
