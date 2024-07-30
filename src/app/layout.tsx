52f33012d44977febc814a996849925e3ebb81c3
이지현 <leejihyun0324@gmail.com>
2024-07-30 21:09:14 +0900

    ✨ Feat(#32): Toast 컴포넌트 구현 (#33)
    * ✨ Feat: toast 컴포넌트 구현
    
    * 🚚 Rename: Form 공통 컴포넌트 폴더명 forms 로 변경
    
    * 🚑️ Fix: document 오류 수정
    
    * 💄 Design: 에러 아이콘 변경
    
    * 🔨 Refactor: 타입이 다를때만 토스트 쌓이게 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import ToastContainer from "@/components/common/toast/container";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +21,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} dark:bg-background-primary dark:text-text-primary`}
       >
+        <ToastContainer />
         {children}
       </body>
     </html>
