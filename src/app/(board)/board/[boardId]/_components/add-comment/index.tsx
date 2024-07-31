43d2a82e9aeb919a72e810b76ecc2ac75027ac41
easyhyun00 <leejihyun0324@gmail.com>
2024-07-31 17:33:31 +0900

    ✨ Feat: 댓글달기 영역 UI 구현

---
+++ src/app/(board)/board/[boardId]/_components/add-comment/index.tsx
@@ -0,0 +1,18 @@
+import { Button, TextArea } from "@/components/common";
+
+const AddComment = () => (
+  <section className="flex flex-col">
+    <h2 className="text-16-700 md:text-20-700">댓글달기</h2>
+    <form className="my-16 md:mt-24">
+      <TextArea id="comment" rows={4} placeholder="댓글을 입력해주세요." />
+    </form>
+    <Button
+      className="h-32 w-74 self-end text-14 md:h-48 md:w-184 md:text-16"
+      variant="primary"
+    >
+      등록
+    </Button>
+  </section>
+);
+
+export default AddComment;
