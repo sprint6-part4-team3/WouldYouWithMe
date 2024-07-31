ee3be19a31008d00a4d6fb24b56b0ff22e766f49
이지현 <leejihyun0324@gmail.com>
2024-07-31 16:10:10 +0900

    💄 Design(#34): 팀 페이지 리포트, 멤버 UI 구현 (#40)
    * 💄 Design: 리포트 개수 디자인 구현
    
    * ✨ Feat: 리포트 진행상황 구현
    
    * ✨ Feat: 팀페이지 멤버 컴포넌트 구현
    
    * ✨ Feat: 초대하기 모달 구현
    
    * ✨ Feat: 유저 프로필 모달 구현
    
    * ✨ Feat: 모바일일때 drawer 보여주기
    
    * 💄 Design: 반응형 및 디자인 수정
    
    * 🔨 Refactor: 변수명 및 컴포넌트 리팩토링
    
    * ✨ Feat: 멤버 추가하기 UI 하단 배치
    
    * 🔨 Refactor: 공통컴포넌트 import index 사용
    
    * 🔨 Refactor: + 문자에서 svg 로 변경
    
    * 🔨 Refactor: hook index 로 묶기
    
    * 🔨 Refactor: 루트레이아웃 main 태그 추가
    
    * 🔨 Refactor: 프로필 모달 아이콘 변경
    
    * 🚚 Rename: teamid 폴더명 임시 변경
    
    * 🚚 Rename: teamid 폴더명 teamId 로변경

---
+++ src/app/[teamId]/_components/member/card.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React from "react";
+
+import { DropDown } from "@/components/common";
+import { useToggle } from "@/hooks";
+import { IconProfileCurrent } from "@/public/assets/icons";
+
+import ProfileModal from "./profile-modal";
+
+// 드롭다운
+const MemberDropdown = () => {
+  const { value, handleToggle, handleOff } = useToggle();
+
+  return (
+    <DropDown handleClose={handleOff}>
+      <DropDown.Trigger onClick={handleToggle}>
+        <span className="cursor-pointer text-16-700 text-text-primary">⋮</span>
+      </DropDown.Trigger>
+      <DropDown.Menu isOpen={value}>
+        <DropDown.Item>삭제하기</DropDown.Item>
+        <DropDown.Item>수정하기</DropDown.Item>
+      </DropDown.Menu>
+    </DropDown>
+  );
+};
+
+const Card = () => {
+  const { value, handleOn, handleOff } = useToggle();
+
+  return (
+    <div className="rounded-16 bg-background-secondary px-16 py-12  md:px-20 md:py-16 lg:px-24 lg:py-20">
+      {/* PC, 테블릿 */}
+      <div className="hidden items-center justify-between md:flex">
+        <div
+          onClick={handleOn}
+          className="group flex cursor-pointer items-center gap-12"
+        >
+          <div className="flex-1">
+            <IconProfileCurrent
+              width={28}
+              height={28}
+              className="size-28 lg:size-32"
+            />
+          </div>
+          <div className="flex flex-1 flex-col gap-2">
+            <span className="group-hover text-14-700">우지은</span>
+            <span className="group-hover text-12-400 text-text-secondary">
+              jieun@codeit.com
+            </span>
+          </div>
+        </div>
+        <MemberDropdown />
+      </div>
+      {/* 모바일 */}
+      <div className="flex items-center justify-between md:hidden">
+        <div onClick={handleOn} className="group cursor-pointer">
+          <div className="flex items-center gap-6">
+            <IconProfileCurrent width={24} height={24} />
+            <span className="group-hover text-14-700">우지은</span>
+          </div>
+          <span className="group-hover text-12-400 text-text-secondary">
+            jieun@codeit.com
+          </span>
+        </div>
+        <MemberDropdown />
+      </div>
+      <ProfileModal isOpen={value} onClose={handleOff} />
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/app/[teamId]/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "경영지원팀",
+  description: "경영지원팀 팀페이지입니다.",
+};
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => (
+  <div className="mx-auto flex max-w-1200 flex-col gap-48">{children}</div>
+);
+
+export default Layout;
