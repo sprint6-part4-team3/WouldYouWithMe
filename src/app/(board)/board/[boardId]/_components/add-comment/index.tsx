ce1c69093e3a54c4eba3bdda955020708bc358d8
easyhyun00 <leejihyun0324@gmail.com>
2024-08-01 17:46:08 +0900

    🔨 Refactor: form 안에 button 넣기

--- src/app/(board)/board/[boardId]/_components/add-comment/index.tsx
+++ src/app/(board)/board/[boardId]/_components/add-comment/index.tsx
@@ -4,16 +4,16 @@ const AddComment = () => (
   <section className="flex flex-col">
     <h2 className="text-16-700 md:text-20-700">댓글달기</h2>
 
-    <form className="my-16 md:mt-24">
+    <form className="mt-16 flex flex-col gap-16 md:mt-24">
       <TextArea id="comment" rows={4} placeholder="댓글을 입력해주세요." />
-    </form>
 
-    <Button
-      className="h-32 w-74 self-end text-14 md:h-48 md:w-184 md:text-16"
-      variant="primary"
-    >
-      등록
-    </Button>
+      <Button
+        className="h-32 w-74 self-end text-14 md:h-48 md:w-184 md:text-16"
+        variant="primary"
+      >
+        등록
+      </Button>
+    </form>
 
     <div className="my-40 h-1 w-full bg-border-primary/10" />
   </section>
