83eefc1df5c3a8ec962f95202076689845a8a19c
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2024-08-02 17:23:57 +0900

    📦️ Chore: 충돌해결

--- src/app/[teamId]/_components/member/add-member-modal.tsx
+++ src/app/[teamId]/_components/member/add-member-modal.tsx
@@ -26,15 +26,6 @@ const AddMemberModal = () => {
         <IconPlusCurrent stroke="#22b8cf" />
         멤버 추가하기
       </div>
-<<<<<<< HEAD
-      {value && (
-        <ModalComponent
-          showCloseButton
-          isOpen={value}
-          onClose={handleOff}
-          title="멤버 초대"
-          description="그룹에 참여할 수 있는 링크를 복사합니다."
-=======
 
       <ModalComponent
         showCloseButton
@@ -47,17 +38,10 @@ const AddMemberModal = () => {
           onClick={handleClickCopy}
           variant="primary"
           className="mt-16 h-47 w-full"
->>>>>>> develop
         >
-          <Button
-            onClick={handleClickCopy}
-            variant="primary"
-            className="mt-16 h-47 w-full"
-          >
-            복사하기
-          </Button>
-        </ModalComponent>
-      )}
+          복사하기
+        </Button>
+      </ModalComponent>
     </>
   );
 };
