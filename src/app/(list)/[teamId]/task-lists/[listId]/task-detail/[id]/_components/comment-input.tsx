e83789e18ffbd08572580fed01a32c891a18df06
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2024-08-02 23:13:12 +0900

    ✨ Feat: 인풋 입력시 아이콘 버튼 색상 변경

--- src/app/(list)/[teamId]/task-lists/[listId]/task-detail/[id]/_components/comment-input.tsx
+++ src/app/(list)/[teamId]/task-lists/[listId]/task-detail/[id]/_components/comment-input.tsx
@@ -1,20 +1,33 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 import { IconButton } from "@/components/common";
 
-const CommentInput = () => (
-  <div className="relative mt-16 border-y-[0.2px] border-text-disabled">
-    <input
-      type="text"
-      placeholder="댓글을 달아주세요"
-      className="w-full rounded-lg border-none bg-background-primary p-13 text-white placeholder:text-gray-400 focus-visible:outline-none"
-    />
-    <IconButton
-      type="submit"
-      icon="IconComment"
-      className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-white"
-    />
-  </div>
-);
+const CommentInput = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <div className="relative mt-16 border-y-[0.2px] border-text-disabled">
+      <input
+        type="text"
+        placeholder="댓글을 달아주세요"
+        className="w-full rounded-lg border-none bg-background-primary p-13 text-white placeholder:text-gray-400 focus-visible:outline-none"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <IconButton
+        type="submit"
+        icon="IconComment"
+        variant={inputValue ? "green" : "darkest"}
+        className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-white"
+      />
+    </div>
+  );
+};
 
 export default CommentInput;
