6702533effff0fe74e632edf056856b1141e1738
이지현 <leejihyun0324@gmail.com>
2024-08-03 12:23:43 +0900

    💄 Design(#50): 팀 없을 경우 팀페이지 UI 구현 (#51)
    * 💄 Design: 팀소속 없을 경우 UI 구현
    
    * ✨ Feat: 버튼 Link 연결 구현
    
    * ✨ Feat: 멤버 json 데이터 연동
    
    * 🔥 Remove: 그룹테스트 json 삭제
    
    * ✅ Test: 팀 없을 때로 수정
    
    * ✨ Feat: 리포트 json 데이터 연결

--- src/app/[teamId]/page.tsx
+++ src/app/[teamId]/page.tsx
@@ -1,11 +1,27 @@
+import { GroupTask } from "@/types/group";
+import { GroupMember } from "@/types/user";
+
+import Empty from "./_components/empty";
 import MemberBox from "./_components/member";
 import ReportBox from "./_components/report";
+// json test data
+import TeamTestData from "./team.json";
 
-const TeamPage = () => (
-  <>
-    <ReportBox />
-    <MemberBox />
-  </>
-);
+const TeamPage = () => {
+  const members: GroupMember[] = TeamTestData.members as GroupMember[];
+  const tasks: GroupTask[] = TeamTestData.taskLists as GroupTask[];
+
+  // 빈배열이면 Empty 화면, 임시로 아무값 넣음
+  const MembershipsTestData = [1];
+
+  return MembershipsTestData.length === 0 ? (
+    <Empty />
+  ) : (
+    <>
+      <ReportBox taskList={tasks} />
+      <MemberBox memberList={members} />
+    </>
+  );
+};
 
 export default TeamPage;
