c71be0c86de44592157cb7adb08faf2a90d75d83
sohee <topa11uu@gmail.com>
2024-08-05 13:00:27 +0900

    💄 Design: 이미지 추가

--- public/assets/images/index.ts
+++ public/assets/images/index.ts
@@ -1,5 +1,13 @@
 export { default as ImgDone } from "./img-done.png";
 export { default as ImgGoogle } from "./img-google.png";
 export { default as ImgKakao } from "./img-kakaotalk.png";
+export { default as landingBottom } from "./img-landing-bottom.png";
+export { default as landingIconFirst } from "./img-landing-icon-1.png";
+export { default as landingIconSecond } from "./img-landing-icon-2.png";
+export { default as landingIconThird } from "./img-landing-icon-3.png";
+export { default as landingMain } from "./img-landing-main.png";
+export { default as landingMockupFirst } from "./img-landing-mockup-1.png";
+export { default as landingMockupSecond } from "./img-landing-mockup-2.png";
+export { default as landingMockupThird } from "./img-landing-mockup-3.png";
 export { default as ImgTodo } from "./img-todo.png";
 export { default as LogoCoworkers } from "./logo-coworkers.png";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+import Image from "next/image";
+
+import {
+  landingBottom,
+  landingIconFirst,
+  landingIconSecond,
+  landingIconThird,
+  landingMain,
+  landingMockupFirst,
+  landingMockupSecond,
+  landingMockupThird,
+} from "@/public/assets/images";
+
 export default function Home() {
-  return <div> Hi! </div>;
+  return (
+    <div>
+      <Image src={landingMain} alt="메인 이미지" />
+      <div>
+        <Image src={landingIconFirst} alt="아이콘1" />
+        <Image src={landingIconSecond} alt="아이콘2" />
+        <Image src={landingIconThird} alt="아이콘3" />
+        <Image src={landingMockupFirst} alt="이미지1" />
+        <Image src={landingMockupSecond} alt="이미지2" />
+        <Image src={landingMockupThird} alt="이미지3" />
+      </div>
+      <Image src={landingBottom} alt="메인 이미지" />
+    </div>
+  );
 }
