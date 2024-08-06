7ee7fb84510726d24f5e013044ad1f9e9d6dbf57
easyhyun00 <leejihyun0324@gmail.com>
2024-08-06 15:50:18 +0900

    🚚 Rename: addteam 에서 create-team 으로 jointeam 에서 join-team 으로 변경

--- src/app/(team-management)/create-team/page.tsx
+++ src/app/(team-management)/create-team/page.tsx
@@ -1,21 +1,21 @@
 import Link from "next/link";
 
-import AddTeamForm from "../_components/add-team-form";
+import CreateTeamForm from "../_components/create-team-form";
 
-const AddTeamPage = () => (
+const CreateTeamPage = () => (
   <>
     <h1 className="text-24-500 md:text-32 lg:text-40">팀 생성하기</h1>
     <p className="mb-24 mt-12 text-14-400 text-text-disabled md:my-36 md:mt-24 md:text-16-400">
       팀 이름은 회사명이나 모임 이름 등으로 설정하면 좋아요.
     </p>
-    <AddTeamForm />
+    <CreateTeamForm />
     <p>
       팀에 참여하고 싶으신가요?
-      <Link className="ml-12 text-brand-primary underline" href="/jointeam">
+      <Link className="ml-12 text-brand-primary underline" href="/join-team">
         팀 참여하기
       </Link>
     </p>
   </>
 );
 
-export default AddTeamPage;
+export default CreateTeamPage;
