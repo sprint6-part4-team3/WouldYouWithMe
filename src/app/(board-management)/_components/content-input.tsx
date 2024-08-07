7723b5fe53a5b9f62c818f852a88c8a11ab973aa
이지현 <leejihyun0324@gmail.com>
2024-08-07 11:48:40 +0900

    💄 Design(#69): 게시글 작성 및 수정 페이지 UI 구현 (#73)
    * 💄 Design: 게시글 쓰기 UI 구현
    
    * 💄 Design: 게시물 쓰기 반응형 구현
    
    * ✨ Feat: 게시글 수정 페이지 구현
    
    * 🚚 Rename: addboard 에서 create-board 로 변경
    
    * 🚚 Rename: addteam 에서 create-team 으로 jointeam 에서 join-team 으로 변경

---
+++ src/app/(board-management)/_components/content-input.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useFormContext } from "react-hook-form";
+
+import { FieldWrapper, TextArea } from "@/components/common";
+import { BoardAddEditInput } from "@/types/article/add-edit";
+
+const ContentInput = () => {
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useFormContext<BoardAddEditInput>();
+
+  return (
+    <FieldWrapper
+      label={
+        <span>
+          <span className="text-brand-tertiary">* </span>
+          내용
+        </span>
+      }
+      id="content"
+      errorMessage={errors.content?.content?.message || ""}
+    >
+      <TextArea
+        {...register("content.content")}
+        id="content"
+        placeholder="내용을 입력해주세요"
+        rows={6}
+        isError={!!errors.content?.content}
+        value={watch("content.content")}
+      />
+    </FieldWrapper>
+  );
+};
+
+export default ContentInput;
---
+++ src/app/(board-management)/_components/form-header.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useFormContext } from "react-hook-form";
+
+import { Button } from "@/components/common";
+import { BoardAddEditInput } from "@/types/article/add-edit";
+
+interface BoardFormHeaderProps {
+  type: "write" | "edit";
+}
+
+const BoardFormHeader = ({ type }: BoardFormHeaderProps) => {
+  const {
+    formState: { isValid },
+  } = useFormContext<BoardAddEditInput>();
+
+  return (
+    <div className="mb-56 flex items-center justify-between">
+      <h1 className="text-18-500 md:text-20-700">
+        게시글 {type === "write" ? "쓰기" : "수정"}
+      </h1>
+      <Button
+        disabled={!isValid}
+        type="submit"
+        variant="primary"
+        className="h-32 w-74 md:h-48 md:w-184"
+      >
+        등록
+      </Button>
+    </div>
+  );
+};
+
+export default BoardFormHeader;
