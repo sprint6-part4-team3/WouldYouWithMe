df6222feac0ffbb26d3e47b83bf68bfb0ed65132
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2024-08-10 20:23:26 +0900

    🔨 Refactor: get-task-detail 에러 처리부분 수정

--- src/lib/api/task-detail/get-task-detail.ts
+++ src/lib/api/task-detail/get-task-detail.ts
@@ -1,3 +1,7 @@
+/* eslint-disable no-console */
+
+import { AxiosError } from "axios";
+
 import { TaskDetailData } from "@/types/task-detail";
 
 import instance from "../axios-instance";
@@ -17,6 +21,20 @@ const getTaskDetail = async (
 
     return response.data;
   } catch (error) {
+    console.error("Error in getTaskDetail:", error);
+    if (error instanceof AxiosError) {
+      if (error.response) {
+        console.error(
+          `태스크 상세 정보를 가져오는데 실패했습니다: ${error.response.status} ${error.response.statusText}`,
+        );
+      } else if (error.request) {
+        console.error("서버로부터 응답을 받지 못했습니다");
+      } else {
+        console.error("요청을 설정하는 중 오류가 발생했습니다");
+      }
+    } else {
+      console.error("예상치 못한 오류가 발생했습니다");
+    }
     return null;
   }
 };
