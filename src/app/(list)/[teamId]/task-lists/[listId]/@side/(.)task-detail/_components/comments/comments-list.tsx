02406d5db1b571fc3a092a82c08e7b1529835079
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2024-08-12 13:19:34 +0900

    🔨 Refactor: 댓글 정렬방식 수정

--- src/app/(list)/[teamId]/task-lists/[listId]/@side/(.)task-detail/_components/comments/comments-list.tsx
+++ src/app/(list)/[teamId]/task-lists/[listId]/@side/(.)task-detail/_components/comments/comments-list.tsx
@@ -10,41 +10,48 @@ interface CommentListProps {
   comments: Comment[];
 }
 
-const CommentList = ({ comments }: CommentListProps) => (
-  <div className="flex min-w-350 flex-col gap-16 pt-24">
-    {comments.map((comment) => (
-      <div
-        key={comment.id}
-        className="flex w-full flex-col items-start gap-16 border-b-[0.2px] border-text-disabled"
-      >
-        <div className="flex items-start justify-between self-stretch text-14-400">
-          {comment.content}
-          <IconButton variant="none" icon="IconKebab" />
-        </div>
-        <div className="mb-12 flex items-center justify-between self-stretch text-14-500 text-text-primary">
-          <div className="flex items-center">
-            {comment.user?.image ? (
-              <Image
-                src={comment.user.image}
-                alt={`${comment.user.nickname}'s profile`}
-                width={24}
-                height={24}
-                className="mr-12 rounded-full"
-              />
-            ) : (
-              <IconProfile className="mr-12" />
-            )}
-            <span className="text-14-500 text-text-primary">
-              {comment.user?.nickname || "Unknown User"}
-            </span>
+const CommentList = ({ comments }: CommentListProps) => {
+  // updatedAt 기준으로 정렬
+  const sortedComments = [...comments].sort((a, b) =>
+    dayjs(b.updatedAt).isAfter(dayjs(a.updatedAt)) ? 1 : -1,
+  );
+
+  return (
+    <div className="flex min-w-350 flex-col gap-16 pt-24">
+      {sortedComments.map((comment) => (
+        <div
+          key={comment.id}
+          className="flex w-full flex-col items-start gap-16 border-b-[0.2px] border-text-disabled"
+        >
+          <div className="flex items-start justify-between self-stretch text-14-400">
+            {comment.content}
+            <IconButton variant="none" icon="IconKebab" />
           </div>
-          <time className="text-14-400 text-text-secondary">
-            {dayjs(comment.createdAt).format("YYYY.MM.DD")}
-          </time>
+          <div className="mb-12 flex items-center justify-between self-stretch text-14-500 text-text-primary">
+            <div className="flex items-center">
+              {comment.user?.image ? (
+                <Image
+                  src={comment.user.image}
+                  alt={`${comment.user.nickname}'s profile`}
+                  width={24}
+                  height={24}
+                  className="mr-12 rounded-full"
+                />
+              ) : (
+                <IconProfile className="mr-12" />
+              )}
+              <span className="text-14-500 text-text-primary">
+                {comment.user?.nickname || "Unknown User"}
+              </span>
+            </div>
+            <time className="text-14-400 text-text-secondary">
+              {dayjs(comment.createdAt).format("YYYY.MM.DD")}
+            </time>
+          </div>
         </div>
-      </div>
-    ))}
-  </div>
-);
+      ))}
+    </div>
+  );
+};
 
 export default CommentList;
