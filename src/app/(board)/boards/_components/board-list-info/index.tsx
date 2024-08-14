77181eaf024e5323c8faa9c0d95cb31de18f8ed6
easyhyun00 <leejihyun0324@gmail.com>
2024-08-14 20:49:57 +0900

    ✨ Feat: 게시물 목록 상단 구현

--- src/app/(board)/boards/_components/board-list-info/index.tsx
+++ src/app/(board)/boards/_components/board-list-info/index.tsx
@@ -4,10 +4,14 @@
 
 import { useState } from "react";
 
+import { OrderType } from "@/constants/board-order-option";
+
 import SearchBar from "./search-bar";
+import TopTitle from "./top-title";
 
 const BoardListInfo = () => {
   const [keyword, setKeyword] = useState("");
+  const [orderBy, setOrderBy] = useState<OrderType>("recent");
 
   const handleSearchItem = (data: string) => {
     setKeyword(data);
@@ -15,8 +19,9 @@ const BoardListInfo = () => {
   };
 
   return (
-    <section>
+    <section className="flex flex-col gap-40">
       <SearchBar onSearchItem={handleSearchItem} />
+      <TopTitle orderBy={orderBy} setOrderBy={setOrderBy} />
     </section>
   );
 };
