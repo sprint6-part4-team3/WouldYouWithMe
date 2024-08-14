6f35d8f21c8b47dccc56318f2fc4c1490184270c
easyhyun00 <leejihyun0324@gmail.com>
2024-08-14 13:41:12 +0900

    ✨ Feat: 토큰 만료 문구 작성

--- src/app/[teamId]/_components/member/redirect-board-modal.tsx
+++ src/app/[teamId]/_components/member/redirect-board-modal.tsx
@@ -20,7 +20,15 @@ const RedirectBoardModal = ({
       showCloseButton
       onClose={handleOff}
       title="모집글 작성"
-      description="복사된 토큰으로 모집글을 작성하시겠습니까?"
+      description={
+        <div className="flex flex-col gap-4">
+          <span>복사된 토큰으로 모집글을 작성하시겠습니까?</span>
+          <span className="text-14-400 text-text-default">
+            (토큰 만료기한은 <span className="text-brand-primary">3일</span>{" "}
+            입니다.)
+          </span>
+        </div>
+      }
     >
       <div className="flex gap-8">
         <Button variant="secondary" onClick={handleOff} className="h-48 w-136">
