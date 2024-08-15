a9d54848643764181abb5ef12ad794e4365cfc75
easyhyun00 <leejihyun0324@gmail.com>
2024-08-15 16:09:44 +0900

    🔨 Refactor: 검색창 form 태그로 변경

--- src/app/(board)/boards/_components/board-list-info/search-bar.tsx
+++ src/app/(board)/boards/_components/board-list-info/search-bar.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, KeyboardEvent, useState } from "react";
+import { ChangeEvent, FormEvent, KeyboardEvent, useState } from "react";
 
 import { IconSearch } from "@/public/assets/icons";
 
@@ -14,27 +14,22 @@ const SearchBar = ({ keyword, onSearchItem }: SearchBarProps) => {
     setInputValue(e.target.value);
   };
 
-  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.nativeEvent.isComposing) {
-      return;
-    }
-    if (e.key === "Enter") {
-      e.preventDefault();
-      onSearchItem(inputValue);
-    }
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSearchItem(inputValue);
   };
 
   return (
-    <div className="relative">
+    <form className="relative" onSubmit={handleSubmit}>
       <IconSearch className="absolute left-12 top-17" />
       <input
         className="h-56 w-full rounded-xl bg-background-secondary py-3.5 pl-42 pr-5 text-16-600 outline-none transition-all duration-300 focus:ring-1 focus:ring-brand-primary"
         placeholder="검색어를 입력해주세요"
         value={inputValue}
         onChange={handleChangeInput}
-        onKeyDown={handleKeyDown}
+        name="keyword"
       />
-    </div>
+    </form>
   );
 };
 
