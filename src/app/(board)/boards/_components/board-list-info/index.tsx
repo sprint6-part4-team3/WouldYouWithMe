5ea78a3b0731c6c4b0a1eeabcfb159406015cdff
easyhyun00 <leejihyun0324@gmail.com>
2024-08-15 21:43:34 +0900

    ✨ Feat: 게시물 목록 API 기능 구현

--- src/app/(board)/boards/_components/board-list-info/index.tsx
+++ src/app/(board)/boards/_components/board-list-info/index.tsx
@@ -1,14 +1,18 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 import { OrderType } from "@/constants/board-order-option";
+import getBoardList from "@/lib/api/board/get-board-list";
+import { BoardListResponse } from "@/types/board-list";
+import scrollToTop from "@/utils/scroll-to-top";
 
 import BoardList from "./board-list";
 import Pagination from "./pagination";
 import SearchBar from "./search-bar";
-import testData from "./test.json";
+import BoardCardSkeleton from "./skeleton";
 import TopTitle from "./top-title";
 
 const BoardListInfo = () => {
@@ -22,6 +26,15 @@ const BoardListInfo = () => {
   const [page, setPage] = useState<number>(
     Number(searchParams.get("page")) || 1,
   );
+
+  const {
+    data: boardListData,
+    isLoading,
+    isError,
+  } = useQuery<BoardListResponse>({
+    queryKey: ["boardList", page, orderBy, keyword],
+    queryFn: () => getBoardList({ page, orderBy, keyword }),
+  });
 
   useEffect(() => {
     const queryPage = searchParams.get("page");
@@ -45,36 +58,56 @@ const BoardListInfo = () => {
     newKeyword: string,
   ) => {
     const orderParam = newOrderBy || orderBy;
-    const keywordParm = newKeyword || "";
+    const keywordParam = newKeyword || "";
 
     router.push(
-      `/boards?page=${newPage}&orderBy=${orderParam}&keyword=${keywordParm}`,
+      `/boards?page=${newPage}&orderBy=${orderParam}&keyword=${keywordParam}`,
       {
-        scroll: false,
+        scroll: true,
       },
     );
   };
 
   const handleSearchItem = (value: string) => {
+    setKeyword(value);
     handleCurrentPage(1, orderBy, value);
   };
 
+  if (isError) {
+    throw new Error("게시물 목록을 가져오지 못했습니다.");
+  }
+
   return (
-    <section className="flex flex-col gap-40">
-      <SearchBar keyword={keyword} onSearchItem={handleSearchItem} />
-      <TopTitle
-        orderBy={orderBy}
-        setOrderBy={(value: OrderType) => handleCurrentPage(1, value, keyword)}
-      />
-      <BoardList boardData={testData.list} />
-      <Pagination
-        totalCount={testData.totalCount}
-        currentPage={page}
-        handleCurrentPage={(newPage) =>
-          handleCurrentPage(newPage, orderBy, keyword)
-        }
-      />
-    </section>
+    <Suspense fallback="로딩중">
+      <section className="flex flex-col gap-40">
+        <SearchBar keyword={keyword} onSearchItem={handleSearchItem} />
+        <TopTitle
+          orderBy={orderBy}
+          setOrderBy={(value: OrderType) =>
+            handleCurrentPage(1, value, keyword)
+          }
+        />
+        {isLoading && (
+          <div className="mb-40 grid grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3 lg:gap-20">
+            {Array.from({ length: 4 }, (_, index) => (
+              <BoardCardSkeleton key={index} />
+            ))}
+          </div>
+        )}
+        {boardListData && (
+          <>
+            <BoardList boardData={boardListData.list} />
+            <Pagination
+              totalCount={boardListData.totalCount}
+              currentPage={page}
+              handleCurrentPage={(newPage) =>
+                handleCurrentPage(newPage, orderBy, keyword)
+              }
+            />
+          </>
+        )}
+      </section>
+    </Suspense>
   );
 };
 
