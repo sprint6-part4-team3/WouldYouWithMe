6570a5599138c77fb6e9645afdfdd2575cfc3db2
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2024-08-17 20:05:32 +0900

    🚑️ Fix: 다른아이디로 와도 이전아이디의 히스토리가 오는버그 수정
    ssr => csr 로 바꿨습니다 ㅠㅠ

--- src/app/user/history/page.tsx
+++ src/app/user/history/page.tsx
@@ -1,13 +1,34 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 
 import NotFound from "@/app/not-found";
+import PageLoading from "@/components/loading/index";
 import getMyHistory from "@/lib/api/my-history/get-my-history";
+import { Task } from "@/types/user-history";
 
 import MyHistory from "./_components/my-history-list";
 
-const MyHistoryPage = async () => {
-  const data = await getMyHistory();
-  const myHistoryData = data.tasksDone;
+const MyHistoryPage = () => {
+  const [myHistoryData, setMyHistoryData] = useState<Task[] | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getMyHistory();
+        setMyHistoryData(data.tasksDone);
+      } catch (error) {
+        console.error("히스토리를 불러오는 중 오류가 발생했습니다:", error); //eslint-disable-line
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) return <PageLoading />;
 
   if (!myHistoryData || myHistoryData.length === 0) {
     return <NotFound />;
