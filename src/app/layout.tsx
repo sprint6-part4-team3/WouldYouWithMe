402bbff5ec4dc111c52822b3d413e2fec2e532da
easyhyun00 <leejihyun0324@gmail.com>
2024-08-17 16:50:53 +0900

    ✨ Feat: 채널톡 기능 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,9 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import AppProvider from "@/components/app-provider";
 import ToastContainer from "@/components/common/toast/container";
 import NavBar from "@/components/nav-bar";
-import ReactQueryProvider from "@/components/react-query-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,12 +20,12 @@ const RootLayout: React.FC<Readonly<{ children: React.ReactNode }>> = ({
     <body
       className={`${inter.className} min-w-330 dark:bg-background-primary dark:text-text-primary`}
     >
-      <ReactQueryProvider>
+      <AppProvider>
         <ToastContainer />
         <NavBar />
         <div className="fixed inset-0 -z-10 bg-[url('/assets/images/img-background.png')] bg-cover bg-center bg-repeat opacity-20" />
         <main className="px-16 ">{children}</main>
-      </ReactQueryProvider>
+      </AppProvider>
     </body>
   </html>
 );
