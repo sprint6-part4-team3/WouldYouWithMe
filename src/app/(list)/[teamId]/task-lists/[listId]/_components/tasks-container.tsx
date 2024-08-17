2cff12392ad42065b9dcd39d72a42dbba46b3450
MR.J <cowhdals1111@gmail.com>
2024-08-17 18:14:54 +0900

    ✨ Feat: dnd 라이브러리 적용

--- src/app/(list)/[teamId]/task-lists/[listId]/_components/tasks-container.tsx
+++ src/app/(list)/[teamId]/task-lists/[listId]/_components/tasks-container.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from "@hello-pangea/dnd";
+
 import { TaskList } from "@/types/group";
 
 import TaskCard from "./task-card";
@@ -8,18 +15,48 @@ interface TasksProps {
   initialTasks: TaskList[];
 }
 
-const TasksContainer = ({ initialTasks }: TasksProps) => (
-  <div>
-    {initialTasks.map((task) => (
-      <TaskCard
-        key={task.id}
-        id={task.id}
-        name={task.name}
-        date={task.date}
-        frequency={task.frequency}
-      />
-    ))}
-  </div>
-);
+const TasksContainer = ({ initialTasks }: TasksProps) => {
+  const onDragEnd = ({ source, destination }: DropResult) => {
+    // 로직 추가
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="tasks" type="tasks">
+        {(droppableProvided) => (
+          <div
+            ref={droppableProvided.innerRef}
+            {...droppableProvided.droppableProps}
+          >
+            {initialTasks.map((task, index) => (
+              <Draggable
+                key={task.id.toString()}
+                draggableId={task.id.toString()}
+                index={index}
+                disableInteractiveElementBlocking={false}
+              >
+                {(draggableProvided) => (
+                  <div
+                    ref={draggableProvided.innerRef}
+                    {...draggableProvided.draggableProps}
+                    {...draggableProvided.dragHandleProps}
+                  >
+                    <TaskCard
+                      id={task.id}
+                      name={task.name}
+                      date={task.date}
+                      frequency={task.frequency}
+                    />
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {droppableProvided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+};
 
 export default TasksContainer;
