26f37b14131448e1bc24ee4abaed75c06d12d524
MR.J <cowhdals1111@gmail.com>
2024-08-19 00:31:07 +0900

    🔨 Refactor: 쿼리 사용

--- src/app/(list)/[teamId]/task-lists/[listId]/_components/tasks-container.tsx
+++ src/app/(list)/[teamId]/task-lists/[listId]/_components/tasks-container.tsx
@@ -1,25 +1,57 @@
 "use client";
 
-import { TaskList } from "@/types/group";
+import { useQuery } from "@tanstack/react-query";
+
+import getTasks from "@/lib/api/task-lists/get-tasks";
 
 import TaskCard from "./task-card";
 
 interface TasksProps {
-  initialTasks: TaskList[];
+  currentTeamId: number;
+  currentDate: Date;
+  currentListId: number;
 }
 
-const TasksContainer = ({ initialTasks }: TasksProps) => (
-  <div>
-    {initialTasks.map((task) => (
-      <TaskCard
-        key={task.id}
-        id={task.id}
-        name={task.name}
-        date={task.date}
-        frequency={task.frequency}
-      />
-    ))}
-  </div>
-);
+const TasksContainer = ({
+  currentTeamId,
+  currentDate,
+  currentListId,
+}: TasksProps) => {
+  const stringCurrentDate = currentDate.toISOString();
+  const { data: tasks } = useQuery({
+    queryKey: ["tasks", currentTeamId, currentListId, stringCurrentDate],
+    queryFn: () =>
+      getTasks({
+        groupId: currentTeamId,
+        taskListId: currentListId,
+        date: stringCurrentDate,
+      }),
+  });
+  if (!tasks) throw new Error();
+  if (tasks?.length === 0) {
+    return (
+      <article className="mb-16 flex w-full flex-col gap-10 rounded-lg bg-background-secondary px-14 py-12">
+        <div className="mb-2 flex items-center justify-between">
+          <div className="flex items-center">
+            <h2>할 일을 추가해 주세요</h2>
+          </div>
+        </div>
+      </article>
+    );
+  }
+  return (
+    <div>
+      {tasks.map((task) => (
+        <TaskCard
+          key={task.id}
+          id={task.id}
+          name={task.name}
+          date={task.date}
+          frequency={task.frequency}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default TasksContainer;
--- src/app/[teamId]/page.tsx
+++ src/app/[teamId]/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
+import { useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 import getGroupData from "@/lib/api/group/get-group-data";
-import { GroupResponse, GroupTask } from "@/types/group";
-import { GroupMember } from "@/types/user";
+import { GroupResponse } from "@/types/group";
 
 import PageLoading from "../../components/loading";
 import Empty from "./_components/empty";
@@ -17,12 +17,17 @@ const TeamPage = ({ params }: { params: { teamId: number } }) => {
   const [userData, setUserData] = useState<GroupResponse | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const { teamId } = params;
-
+  const queryClient = useQueryClient();
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         const res: GroupResponse = await getGroupData(teamId);
         setUserData(res);
+        const taskLists = res.taskLists.map(({ id, name }) => ({
+          id,
+          name,
+        }));
+        queryClient.setQueryData(["task-lists", Number(teamId)], taskLists);
       } catch (e) {
         // eslint-disable-next-line no-console
         console.error("error:", e);
