689887daafd752c35371f7aec9ab15a8124c1361
easyhyun00 <leejihyun0324@gmail.com>
2024-08-19 18:10:46 +0900

    ✨ Feat: 그래프 애니메이션 적용

--- src/app/team/[teamId]/_components/report/todo-count.tsx
+++ src/app/team/[teamId]/_components/report/todo-count.tsx
@@ -9,7 +9,9 @@ const TodoCount = ({ title, count }: TodoCountProps) => (
   <div className="flex h-80 items-center justify-between rounded-12 bg-background-tertiary p-16">
     <div className="flex flex-col gap-4">
       <span className="text-12-500 text-text-secondary">{title}</span>
-      <span className="text-24-700 text-brand-tertiary">{count}개</span>
+      <span className="text-20-700 text-brand-tertiary md:text-24-700">
+        {count}개
+      </span>
     </div>
 
     <Image
