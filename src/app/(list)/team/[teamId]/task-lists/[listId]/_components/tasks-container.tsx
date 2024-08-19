c294a7110c654359f9801d873b2f42acd3b159bb
MR.J <cowhdals1111@gmail.com>
2024-08-19 18:39:50 +0900

    ✨ Feat: api 연결

--- src/app/(list)/team/[teamId]/task-lists/[listId]/_components/tasks-container.tsx
+++ src/app/(list)/team/[teamId]/task-lists/[listId]/_components/tasks-container.tsx
@@ -8,6 +8,7 @@ import {
 } from "@hello-pangea/dnd";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 
+import editTasksOrder from "@/lib/api/task-lists/edit-tasks-order";
 import getTasks from "@/lib/api/task-lists/get-tasks";
 
 import TaskCard from "./task-card";
@@ -47,7 +48,7 @@ const TasksContainer = ({
     );
   }
 
-  const onDragEnd = ({ source, destination }: DropResult) => {
+  const onDragEnd = async ({ source, destination }: DropResult) => {
     if (!destination) return; // 목적지가 없으면 아무 작업도 하지 않음
 
     // 순서 변경이 없을 때
@@ -61,9 +62,17 @@ const TasksContainer = ({
     const [movedTask] = reorderedTasks.splice(source.index, 1);
     reorderedTasks.splice(destination.index, 0, movedTask);
 
+    // 옵티미스틱
     queryClient.setQueryData(
       ["tasks", currentTeamId, currentListId, stringCurrentDate],
       reorderedTasks,
+    );
+    // api 호출
+    await editTasksOrder(
+      currentTeamId,
+      currentListId,
+      movedTask.id,
+      destination.index,
     );
   };
   return (
