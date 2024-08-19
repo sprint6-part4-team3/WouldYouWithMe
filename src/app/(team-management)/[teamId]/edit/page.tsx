10f7aa69d9a71dfe566f1a9bb8524c5673e7e944
easyhyun00 <leejihyun0324@gmail.com>
2024-08-19 14:48:32 +0900

    ✨ Feat: 어드민일때 수정페이지 접근가능 구현

--- src/app/(team-management)/[teamId]/edit/page.tsx
+++ src/app/(team-management)/[teamId]/edit/page.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable consistent-return */
 
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 import getGroupData from "@/lib/api/group/get-group-data";
+import getTeamAdmin from "@/utils/get-team-admin";
 
 import EditTeamForm from "../../_components/edit-team-form";
 
@@ -14,21 +15,23 @@ const EditTeamPage = async ({ params }: { params: { teamId: number } }) => {
   try {
     const res = await getGroupData(teamId);
 
-    if (res.members[0]?.userId !== Number(userId)) {
-      return redirect("/not-found");
+    const adminId = getTeamAdmin(res.members);
+
+    if (adminId !== Number(userId)) {
+      notFound();
     }
 
     return (
       <>
         <h1 className="text-24-500 md:text-32 lg:text-40">팀 수정하기</h1>
         <p className="mb-24 mt-12 text-14-400 text-text-disabled md:my-36 md:mt-24 md:text-16-400">
-          팀 이름은 회사명이나 모임 이름 등으로 설정하면 좋아요.
+          팀 이름은 스터디명이나 모임 이름 등으로 설정하면 좋아요.
         </p>
         <EditTeamForm id={teamId} name={res.name} image={res.image} />
       </>
     );
   } catch (error) {
-    return redirect("/error");
+    throw new Error("에러발생");
   }
 };
 
