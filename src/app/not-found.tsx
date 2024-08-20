9d8271dea345906cbe7220aff3c79dcc8fc5bf7d
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2024-08-20 18:39:55 +0900

    ✨ Feat: 404페이지 제작

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,31 @@
-const NotFound = () => (
-  <div>
-    <h1>404</h1>
-    {/** 추후에 404 에러 페이지 만들기 */}
-  </div>
-);
+"use client";
+
+import Lottie from "lottie-react";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/common";
+import notfound from "@/public/assets/lotties/404.json";
+
+const NotFound = () => {
+  const router = useRouter();
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <Lottie
+        animationData={notfound}
+        style={{ width: "45%", height: "45%" }}
+      />
+      <Button
+        variant="primary"
+        className="h-45 w-200"
+        onClick={() => {
+          router.push("/");
+        }}
+      >
+        홈으로 이동
+      </Button>
+    </div>
+  );
+};
 
 export default NotFound;
