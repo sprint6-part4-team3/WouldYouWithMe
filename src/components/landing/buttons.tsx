6143a2eed1444c74f9970c5a9385f91d02646424
MR.J <cowhdals1111@gmail.com>
2024-08-21 19:44:59 +0900

    💄 Design: 버튼 색 변경

--- src/components/landing/buttons.tsx
+++ src/components/landing/buttons.tsx
@@ -7,15 +7,15 @@ const LandingButtons = () => (
   <div className="flex flex-col gap-10 md:flex-row">
     <Link href="/login">
       <Button
-        className="m-auto h-45 w-200 rounded-full text-16-600 hover:bg-gradient-to-r hover:from-brand-tertiary hover:to-brand-primary"
-        variant="primary"
+        className="m-auto h-45 w-200 rounded-full text-16-600 hover:bg-gradient-to-r hover:from-brand-tertiary hover:to-brand-primary hover:text-text-inverse"
+        variant="noFill"
       >
         지금 시작하기
       </Button>
     </Link>
     <Link href="/boards">
       <Button
-        className="m-auto h-45 w-200 rounded-full text-16-600 "
+        className="m-auto h-45 w-200 rounded-full text-16-600 hover:bg-gradient-to-r hover:from-brand-tertiary hover:to-brand-primary hover:text-text-inverse"
         variant="noFill"
       >
         둘러보기
