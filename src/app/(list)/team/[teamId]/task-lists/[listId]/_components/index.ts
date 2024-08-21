139901ee247d1e9838eca3ef28d4a8a7bec89846
MR.J <86304360+JayChae@users.noreply.github.com>
2024-08-21 18:11:46 +0900

    🚑️ Fix(#166): 수정/삭제 버그 해결 (#174)
    * 🚑️ Fix: 수정시 완료 풀리는 버그 수정
    
    * ✨ Feat: 삭제하기 모달
    
    * 🔨 Refactor: 삭제 모달 리팩
    
    * 🔨 Refactor: import문 수정

--- src/app/(list)/team/[teamId]/task-lists/[listId]/_components/index.ts
+++ src/app/(list)/team/[teamId]/task-lists/[listId]/_components/index.ts
@@ -4,6 +4,7 @@ export { default as EditTaskModal } from "./edit-task-modal";
 export { default as TaskCard } from "./task-card";
 export { default as TaskCurrent } from "./task-current";
 export { default as TaskDateNav } from "./task-date-nav";
+export { default as TaskDeleteModal } from "./task-delete-modal";
 export { default as TaskListNav } from "./task-lists-nav";
 export { default as TasksContainer } from "./tasks-container";
 export { default as TasksSkeleton } from "./tasks-skeleton";
