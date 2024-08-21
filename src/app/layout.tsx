b47c9df8d198efdd8e1051f3c8633ae92c4c1cc3
HANSANGWOO <99471821+Han-wo@users.noreply.github.com>
2024-08-21 19:34:57 +0900

    🔨 Refactor(#168): 메타데이타 404 500 페이지 (#187)
    * 🔨 Refactor: 일부 메타데이터 설정
    
    * ✨ Feat: 404페이지 제작
    
    * 🔨 Refactor: 드로우어 x버튼 제거
    
    * ✨ Feat: 500 페이지 제작
    
    * 🔨 Refactor: 메타데이타 수정
    
    * 🚑️ Fix: 빌드오류 해결
    
    ---------
    
    Co-authored-by: han-wo98 <99472476+han-wo98@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,11 @@ import NavBar from "@/components/nav-bar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Coworkers",
+  title: "우주윗미 | 같이 스터디해요",
+  description: "WojuWithMe는 같이 스터디할 사람을 찾아주는 서비스입니다.",
+  icons: {
+    icon: "/icon.ico",
+  },
 };
 
 const RootLayout: React.FC<Readonly<{ children: React.ReactNode }>> = ({
