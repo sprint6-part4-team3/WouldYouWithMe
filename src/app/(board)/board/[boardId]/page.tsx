6575b3678323297f689371f84a6f811669bd2974
easyhyun00 <leejihyun0324@gmail.com>
2024-08-21 13:35:47 +0900

    ✅ Test: 원래대로 돌리기

--- src/app/(board)/board/[boardId]/page.tsx
+++ src/app/(board)/board/[boardId]/page.tsx
@@ -3,10 +3,8 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
-import Head from "next/head";
 import { cookies } from "next/headers";
 
-import EMPTY_IMAGE from "@/constants/image";
 import getBoardDetailData from "@/lib/api/board/get-board-detail-data";
 import getBoardComment from "@/lib/api/board-comment/get-comment";
 
@@ -19,7 +17,7 @@ const BoardPage = async ({ params }: { params: { boardId: number } }) => {
   const { boardId } = params;
   const userId = cookies().get("userId")?.value;
 
-  const [boardData] = await Promise.all([
+  await Promise.all([
     queryClient.fetchQuery({
       queryKey: ["board", boardId],
       queryFn: () => getBoardDetailData(boardId),
@@ -32,25 +30,10 @@ const BoardPage = async ({ params }: { params: { boardId: number } }) => {
   ]);
 
   return (
-    <>
-      <Head>
-        <meta property="og:title" content={boardData?.title ?? "게시판"} />
-        <meta
-          property="og:description"
-          content={boardData?.content ?? "게시물 내용"}
-        />
-        <meta property="og:image" content={boardData?.image || EMPTY_IMAGE} />
-        <meta
-          property="og:url"
-          content={`https://example.com/boards/${boardId}`}
-        />
-      </Head>
-
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <BoardDetail userId={Number(userId)} boardId={boardId} />
-        <CommentList boardId={boardId} />
-      </HydrationBoundary>
-    </>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <BoardDetail userId={Number(userId)} boardId={boardId} />
+      <CommentList boardId={boardId} />
+    </HydrationBoundary>
   );
 };
 
