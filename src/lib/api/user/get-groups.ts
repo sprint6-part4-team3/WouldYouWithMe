de7ee8caddd685947af85a76e3694ec9b525025e
MR.J <cowhdals1111@gmail.com>
2024-08-22 15:45:09 +0900

    ✨ Feat: 참여 중인  팀 목록 페잊;

---
+++ src/lib/api/user/get-groups.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosResponse } from "axios";
+
+import { OTHER_TYPE_ERROR } from "@/constants/error-message";
+import { Group } from "@/types/user";
+
+import instance from "../axios-instance";
+
+// 참여하고 있는 팀
+const getGroups = async () => {
+  try {
+    const { data }: AxiosResponse<Group[]> = await instance.get("/user/groups");
+    return data;
+  } catch (e: unknown) {
+    if (axios.isAxiosError(e)) {
+      throw e.response?.data;
+    } else {
+      throw new Error(OTHER_TYPE_ERROR);
+    }
+  }
+};
+
+export default getGroups;
